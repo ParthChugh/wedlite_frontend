206be6bb401e0a7402e32a88526ac1f9ce6badbf
Parth Chugh <parthchugh52@gmail.com>
2020-05-10 16:29:13 +0530

    added cities

--- src/App.js
+++ src/App.js
@@ -2,50 +2,60 @@ import React, {useState} from 'react';
 import logo from './wedlite.png';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer, toast } from 'react-toastify';
+import { useForm } from 'react-hook-form'
 import SearchBar from './Components/common/SearchBar';
-import {Card, Modal} from 'react-bootstrap';
+// import 'semantic-ui-css/semantic.min.css';
+import {Card, Modal, Button} from 'react-bootstrap';
+import {  } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { CATEGORY, NORMAL} from './constants';
-import { REGISTER_API } from './urls';
+import { REGISTER_API, LOGIN_API } from './urls';
 
 const App = () => {
   const defaultPlace = 'Udaipur'
+  
   const [place, updatePlace] = useState(defaultPlace);
+  
+  const { register, handleSubmit, errors } = useForm()
+
   const [SignUpShow, setSignUpShow] = useState(false);
+
   const goToAppLink = () => {
     console.log("App link is clicked")
   }
 
-  const RegisterUser = () => {
-    console.log('yahan aaya ')
-    const formData = {
-      "email": "user@example.com",
-      "password": "string"
-    };
+  const RegisterUser = (data) => {
     fetch(REGISTER_API, {
       method: 'POST', 
-      body: formData,
+      body: JSON.stringify(data),
       headers: {
         'Content-Type': 'application/json',
       }
+    })
+      .then((response) => {
+        if(response.status === 201) {
+          response.json().then((json) => {
+            toast("Welcome")
+          })
+        } else {
+          toast("Some problem please contact supportas")
+        }
+      })
+      .catch(() => {
+
+    });
+  }
+  const loginUser = (data) => {
+    fetch(LOGIN_API, {
+      method: 'POST', 
+      body: JSON.stringify(data),
     })
       .then((response) => response.json().then((json) => {
         console.log(json);
       }))
       .catch(() => {
-        
     });
-  }
-  const loginUser = (event) => {
-    console.log(event);
-    // fetch(LOGIN_API, {
-    //   method: 'POST', 
-    //   body: data,
-    // })
-    //   .then((response) => response.json().then((json) => {
-
-    //   }))
-    //   .catch(() => {
-    // });
   }
 
   const handleClose = () => {
@@ -89,7 +99,7 @@ const App = () => {
         <img src={logo} alt="logo" className="App-logo" />
         <div style={{marginTop: -50}}>
           <span style={{fontSize: CATEGORY}}>
-            Search the next place with your loved ones in  <span className="font-bold ">{place === '' ? defaultPlace: place}</span>
+            Commission free wedding planner in  <span className="font-bold ">{place === '' ? defaultPlace: place}</span>
           </span>
           <SearchBar
             handleSearch={handleSearch}
@@ -108,6 +118,73 @@ const App = () => {
 
   const [show, setShow] = useState(false);
 
+  const showSignUpModal = () => (
+    <Modal show={SignUpShow}  onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Sign Up</Modal.Title>
+      </Modal.Header>
+      <form className="container margin-top-10" onSubmit={handleSubmit(RegisterUser)}>  
+        <div className="form-group">
+          <label>Email address</label>
+          <input name="email"  className="form-control" placeholder="Enter your email" ref={
+            register({
+              required: true, 
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Invalid email address"
+              }
+              })} />
+            {errors.email && <span style={{color: 'red'}}>Please enter a valid email</span>}
+        </div>
+        <div className="form-group">
+        <label>Password</label>
+          <input  type="password" className="form-control" name="password" placeholder="Password" autoComplete="current-password" ref={register({required: true})} />
+          {errors.password && <span style={{color: 'red'}}>Please enter a valid password</span>}    
+        </div>
+        <Button className="btn btn-primary btn-block" type="submit" variant="outline-dark">
+          Submit
+        </Button>
+        <div className="forgot-password row container margin-vertical-10" >
+          Already Registered? &nbsp;
+          <div style={{cursor:'pointer', color: '#3366BB'}} onClick={handleShow}>Login</div>
+        </div>        
+      </form>
+    </Modal>
+  )
+
+  const showLoginModal = () => (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header className="font-bold" style={{fontSize: CATEGORY}} closeButton>
+        <div>Login</div>
+      </Modal.Header>
+      <form className="container margin-top-10" onSubmit={handleSubmit(loginUser)}>  
+        <div className="form-group">
+          <label>Email address</label>
+          <input name="email"  className="form-control" placeholder="Enter your email" ref={
+            register({
+              required: true, 
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Invalid email address"
+              }
+            })} />
+            {errors.email && <span style={{color: 'red'}}>Please enter a valid email</span>}    
+        </div>
+        <div className="form-group">
+        <label>Password</label>
+          <input  type="password" className="form-control" name="password" placeholder="Password" autoComplete="current-password" ref={register({required: true})} />
+          {errors.password && <span style={{color: 'red'}}>Please enter a valid password</span>}    
+        </div>
+        <Button className="btn btn-primary btn-block" type="submit" variant="outline-dark">
+          Submit
+        </Button>
+        <div className="forgot-password row container margin-vertical-10" >
+          New to WedLite? &nbsp;
+          <div style={{cursor:'pointer', color: '#3366BB'}} onClick={handleSignUpShow}>Create Account</div>
+        </div>        
+      </form>
+    </Modal>
+  )
   return (
     <div>
       <div className="image-background">
@@ -116,67 +193,9 @@ const App = () => {
           {LogoSearchBar()}
         </div>
       </div>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header className="font-bold" style={{fontSize: CATEGORY}} closeButton>
-          <div>Login</div>
-        </Modal.Header>
-        <form className="container margin-top-10" onSubmit={loginUser}>
-          <div className="form-group">
-            <label>Email address</label>
-            <input type="email" className="form-control" placeholder="Enter email" />
-          </div>
-
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" className="form-control" placeholder="Enter password" />
-          </div>
-
-          <div className="form-group">
-            <div className="custom-control custom-checkbox">
-              <input type="checkbox" className="custom-control-input" id="customCheck1" />
-              <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-            </div>
-          </div>
-
-          <button type="submit" className="btn btn-primary btn-block">Submit</button>
-          
-          <div className="forgot-password row container margin-vertical-10" >
-            
-            New to WedLite? &nbsp;
-          
-            <div style={{cursor:'pointer', color: '#3366BB'}} onClick={handleSignUpShow}>Create Account</div>
-            
-          </div>          
-        </form>
-      </Modal>
-      <Modal show={SignUpShow}  onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Sign Up</Modal.Title>
-        </Modal.Header>
-        <form className="container margin-top-10" onSubmit={RegisterUser}>
-          <div className="form-group">
-              <label>Email address</label>
-              <input type="email" className="form-control" placeholder="Enter email" />
-          </div>
-
-          <div className="form-group">
-              <label>Password</label>
-              <input type="password" className="form-control" placeholder="Enter password" />
-          </div>
-
-          <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-          
-          
-            <div className="forgot-password row container margin-vertical-10" >
-              <div>
-              Already registered &nbsp;
-              </div>
-              <div style={{cursor:'pointer', color: '#3366BB'}} onClick={handleShow}>
-                Sign in?
-              </div>
-            </div>          
-        </form>
-      </Modal>
+      {showLoginModal()}
+      {showSignUpModal()}
+      <ToastContainer />
       <div className="row space-around" style={{marginLeft: 100, marginRight: 100, marginTop: 50, marginBottom: 50}}>
         {cards.map((card, index) => (
           <Card style={{ width: '18rem', borderRadius: 10,elevation: 5 }} key={index}>
--- src/Components/common/SearchBar.js
+++ src/Components/common/SearchBar.js
@@ -1,45 +1,84 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Dropdown } from 'semantic-ui-react'
+import {  toast } from 'react-toastify';
+import {CITY_LIST_API} from '../../urls';
 import './SearchBar.css';
-class SearchBar extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      search: ''
-    }
-  }  
+const SearchBar = (props) => {
+  const [addressDefinitions, updateAddressDefinitions] = useState([]);
+  const [selectedCity, updateCity] = useState('');
+  const fetchCities = () => {
+    fetch(CITY_LIST_API, {
+      method: 'GET', 
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => {
+        if(response.status === 200) {
+          response.json().then((json) => {
+            updateAddressDefinitions(json);
+          })
+        } else {
+          toast("Contact Support")
+        }
+      })
+      .catch(() => {
 
-  render () {
-    const handleSearchData = (e) => {
-      this.setState({ search: e.target.value })
-    }
-  
-    const handleGoClick = () => {
-      console.log(this.state.search);
-      this.props.handleSearch(this.state.search);
-    }
-    return (
-      <div className='searchbar-container'>
-        <form onSubmit={e => e.preventDefault()} className="align-items-center">
-          <input
-            type='text'
-            className="searchbar"
-            size='100'
-            placeholder='Search for gardern, vatika, Banquet halls or a place'
-            onChange={handleSearchData}
-            value={this.state.search} />
-          <button
-           type='submit'
-           className="button"
-           onClick={handleGoClick}
-           >
-            <span>
-              Search
-            </span>
-           </button>
-        </form>
-      </div>
-    )
+    });
   }
+  useEffect(() => {
+    fetchCities()
+  },[]);
+
+
+  const [search, updateSearch] = useState('');  
+  const cityOptions = addressDefinitions.map((element, index) => ({
+    key: element.id,
+    text: element.city,
+    value: element.id,
+  }))
+  
+  const handleSearchData = (e) => {
+    updateSearch(e.target.value);
+  }
+
+  const handleGoClick = () => {
+    props.handleSearch(selectedCity);
+  }
+  
+  const updateSelectedCity = (event, data) => {
+    const newData = addressDefinitions.filter(el => el[data.value] === el.value);
+    updateCity(newData[0].city)
+  } 
+
+  return (
+    <div className='searchbar-container'>
+      <form onSubmit={e => e.preventDefault()} className="align-items-center">
+        <Dropdown 
+          className="dropdown"
+          placeholder='cities' 
+          search selection
+          onChange={updateSelectedCity} 
+          options={cityOptions} />
+        <input
+          type='text'
+          className="searchbar"
+          placeholder='Search for gardern, vatika, Banquet halls or a place'
+          onChange={handleSearchData}
+          value={search} />
+        <button
+          type='submit'
+          className="button"
+          onClick={handleGoClick}
+          >
+          <span>
+            Search
+          </span>
+          </button>
+      </form>
+    </div>
+  )
+  
 }
 
 export default SearchBar
