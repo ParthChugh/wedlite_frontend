929099723ae6e67cc3c64b30f3854eb162363e29
Parth Chugh <parthchugh52@gmail.com>
2020-07-13 14:44:13 +0530

    added wedlite shop more products

--- src/Components/Shop/Products.js
+++ src/Components/Shop/Products.js
@@ -5,71 +5,113 @@ import {bindActionCreators} from 'redux';
 import * as ShopActionsCreator from '../../actions/shopActions';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Segment } from 'semantic-ui-react'
+import Loader from 'react-loader-spinner'
 import {useHistory} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Layout from '../Layout';
+import { GET_SHOP_DATA } from '../../urls';
 import paragraph from '../../assets/paragraph.png'
 
 const Home = (props) =>  {
   const { ShopActions: {getItems}, shop } = props;
-  const items = shop.getIn(['items', 'results']);
+  // const items = shop.getIn(['items', 'results']);
+  const [visible, updateVisible] = useState(false);
+  const [items, updateItems] = useState([]);
+  const [nextUrl, updateNextUrl] = useState('');
   const history = useHistory();
+  const getShopItems = (url) => {
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => {
+        response.json().then((json) => { 
+          updateNextUrl(json.next);
+          updateItems(searches => searches.concat(json.results))
+          updateVisible(false);
+        })
+      })
+      .catch(() => {
+    });
+  }
+
   useEffect(() => {
-    getItems()
+    // getItems()
+    getShopItems(GET_SHOP_DATA);
   },[]);
 
   const goToNextScreen = (id) => {
     history.push(`/shop/${id}`)
   }
-  
+
+  const getMoreData = () => {
+    updateVisible(true);
+    getShopItems(nextUrl);
+  }
+    
   return (
     <Layout
       showSearchBar={false}
     >
       <>
-      <div className="row space-around">
-      {
-        items.size > 0 ? items.entrySeq().map((el, index) => {
-          return (  
-            <div 
-              className="card-category"
-              style={{ 
-                margin: 10, 
-                width: '21rem', 
-                borderRadius: 5,
-                cursor: 'pointer', 
-                borderRadius: 20,
-              }}  
-              key={index}
-              onClick={() => goToNextScreen(el[1].get('id'))}  
-            >
-              <LazyLoadImage
-                style={{width: '100%',height: 200, display: 'flex', borderRadius: 20}}
-                className="image-category"
-                alt="display photo"
-                effect="blur"
-                src={el[1].getIn(['photos', 0,'path'])} 
-              />
-              <div style={{paddingLeft: 10}}>
-                <div>
-                  {el[1].getIn(['name'])}                
-                </div>
-                <div>
-                  <div style={{flex: 1, justifyContent: 'center'}}>
-                    ₹ {el[1].getIn(['price'])}
+      <div>
+        <div className="row space-around">
+          {
+            items.length > 0  ? items.map((el, index) => {
+              return (  
+                <div 
+                  className="card-category"
+                  style={{ 
+                    margin: 10, 
+                    width: '21rem', 
+                    borderRadius: 5,
+                    cursor: 'pointer', 
+                    borderRadius: 20,
+                  }}  
+                  key={index}
+                  onClick={() => goToNextScreen(el.id)}  
+                >
+                  <LazyLoadImage
+                    style={{width: '100%',height: 200, display: 'flex', borderRadius: 20}}
+                    className="image-category"
+                    alt="display photo"
+                    effect="blur"
+                    src={el.photos[0].path} 
+                  />
+                  <div style={{paddingLeft: 10}}>
+                    <div>
+                      {el['name']}                
+                    </div>
+                    <div>
+                      <div style={{flex: 1, justifyContent: 'center'}}>
+                        ₹ {el['price']}
+                      </div>
+                  </div>              
                   </div>
-              </div>              
-              </div>
+                </div>
+              )
+            }
+          ) :
+          <div className="row space-around" style={{ marginTop: 'auto' }}>
+              <Segment attached>
+                <img alt="loading" src={paragraph} />
+              </Segment>
             </div>
-          )
-        }
-      ):
-       <div className="row space-around" style={{ marginTop: 'auto' }}>
-          <Segment attached>
-            <img alt="loading" src={paragraph} />
-          </Segment>
+          }
         </div>
-      }
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Loader visible={visible} type="Oval" color='#EA555D' height={80} width={80} />
+        </div> 
+        {
+          nextUrl !== null && !visible ?
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <button className="see-more-button" onClick={getMoreData}>
+              See More
+            </button>
+          </div> : <div />
+        }
       </div>
       </>
     </Layout>
