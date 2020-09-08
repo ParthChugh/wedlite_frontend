6ff0c147cc241cc2041328f0ba8ba37987433f6a
Parth Chugh <parthchugh52@gmail.com>
2020-09-09 03:04:39 +0530

    crawl issue fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {hydrate} from 'react-dom';
 import './index.css';
 import App from './Routes';
 import { Provider } from 'react-redux'
+
 import store, { persistor } from "./store";
 import { PersistGate } from 'redux-persist/integration/react'
 import * as serviceWorker from './serviceWorker';
@@ -16,17 +17,14 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 const trackingId = "UA-166180631-1"; // Replace with your Google Analytics tracking ID
 ReactGA.initialize(trackingId);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root')
-);
+hydrate(<Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </PersistGate>
+</Provider>,
+document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
